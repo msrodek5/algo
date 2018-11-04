372f3b0ae1e69e684d5fda8ac6b63fd52581645b
mariusz srodek <mariusz.srodek@gft.com>
2018-11-04 19:48:42 +0100

    interim commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace algo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new int[] {1, 2, 3, 3, 2, 1, 4} ;
+
+            var capitalIndex = findCapitalCity(cities);
+
+            
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int findCapitalCity(int[] cities)
+        {
+            var capitalIndex = -1;
+
+            return capitalIndex;
+        }
+
+        
+    }
+}
